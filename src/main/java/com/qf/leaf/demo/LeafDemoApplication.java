d16fe3e35985b0866008df65f2eab670966e739c
Jerry7901 <790165133@qq.com>
2021-09-26 21:15:25 +0800

    fisrt demo

---
+++ src/main/java/com/qf/leaf/demo/LeafDemoApplication.java
@@ -0,0 +1,13 @@
+package com.qf.leaf.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeafDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qf/leaf/demo/LeafDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.leaf.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
